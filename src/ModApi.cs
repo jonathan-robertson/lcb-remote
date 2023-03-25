994e10ca8b057944f42904d15615ccc88b17333e
jonathan-robertson <jrob356@icloud.com>
2023-03-25 13:08:09 -0500

    add mod api and log classes

---
+++ src/ModApi.cs
@@ -0,0 +1,14 @@
+﻿namespace LcbRemote
+{
+    internal class ModApi : IModApi
+    {
+        private static readonly ModLog<ModApi> _log = new ModLog<ModApi>();
+
+        public static bool DebugMode { get; set; } = true; // TODO: disable before release
+
+        public void InitMod(Mod _modInstance)
+        {
+            _log.Info("Not yet implemented.");
+        }
+    }
+}
