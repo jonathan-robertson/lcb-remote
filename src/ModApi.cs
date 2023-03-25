1a9d46c0ac4afb5a7e1e7145c1e5bc504274adae
jonathan-robertson <jrob356@icloud.com>
2023-03-25 15:02:40 -0500

    cache key values for world on game start

--- src/ModApi.cs
+++ src/ModApi.cs
@@ -1,14 +1,38 @@
-﻿namespace LcbRemote
+﻿using System;
+
+namespace LcbRemote
 {
     internal class ModApi : IModApi
     {
         private static readonly ModLog<ModApi> _log = new ModLog<ModApi>();
 
         public static bool DebugMode { get; set; } = true; // TODO: disable before release
+        public static bool IsServer { get; private set; }
+        public static int LandClaimSize { get; private set; }
+        public static int LandClaimRadius { get; private set; }
 
         public void InitMod(Mod _modInstance)
         {
-            _log.Info("Not yet implemented.");
+            ModEvents.GameStartDone.RegisterHandler(OnGameStartDone);
+        }
+
+        private void OnGameStartDone()
+        {
+            try
+            {
+                IsServer = SingletonMonoBehaviour<ConnectionManager>.Instance.IsServer;
+                if (IsServer)
+                {
+                    _log.Trace("OnGameStartDone");
+                    LandClaimSize = GameStats.GetInt(EnumGameStats.LandClaimSize); // 41 is the default, for example
+                    LandClaimRadius = LandClaimSize % 2 == 1 ? (LandClaimSize - 1) / 2 : LandClaimSize / 2;
+                    _log.Debug($"LandClaimSize: {LandClaimSize}, LandClaimRadius: {LandClaimRadius}");
+                }
+            }
+            catch (Exception e)
+            {
+                _log.Error("Failed OnGameStartDone", e);
+            }
         }
     }
 }
